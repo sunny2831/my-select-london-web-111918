8b2b558a5c9a3ac74ccc8ca795ef5a1f32042155
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-10 13:46:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
-def my_select(array)
-  i = 0
-  empty_selection = []
-  while i < array.length
-    empty_selection.push yield(array[i])
-    i += 1
-  end
-  empty_selection
-end
+def my_select(array) 
+count = 0
+result = []
 
-my_select(nums) {|num|
-  num.even?}
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
+end
